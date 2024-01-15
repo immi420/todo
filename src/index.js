bf8b688d3ccb628ff740f88729cfc0971f7b9273
Imtiaz Ahmad <imtiazjan10@gmail.com>
2024-01-16 00:28:16 +0500

    mongo db connection

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const express =require('express');
+const mongoose=require('mongoose');
 
 const app = express();
 app.use(express.json());
 const port=3000;
 
+mongoose.connect('mongodb+srv://imtiaz:test123@cluster.s17ywo8.mongodb.net/?retryWrites=true&w=majority');
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,'MongoDB connection error'));
+db.once('open',()=>{
+    console.log('Connected to MongoDb');
+})
+
+
 
 //home
-app.get('/', (req, res) => {
-    res.send("To-Do");
+app.get('/home', (req, res) => {
+    res.json({message: 'Todo is Ready'});
 });
 
 
 app.listen(port, () => {
-    console.log('Server ready at : 3000');
+    console.log(`Server ready at : ${port}`);
 })
