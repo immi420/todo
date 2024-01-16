47c1c2495186e1f2473cd967e1a9adbb792e81d1
Imtiaz Ahmad <imtiazjan10@gmail.com>
2024-01-17 00:54:34 +0500

    getTodos API getTodosByID API created

--- src/controllers/todoControllers.js
+++ src/controllers/todoControllers.js
@@ -12,5 +12,26 @@ exports.createTodo = async (req, res) => {
   }
 };
 
+exports.getTodoById= async(req,res)=>{
+    try {
+      const todo=await Todo.findById(req.params.id);
+      if(!todo){
+        return res.status(404).json({error:"Todo not found"});
+      }
+      res.json(todo);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getTodos= async(req,res)=>{
+  try {
+    const todo=await Todo.find();
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 
 // Implement other CRUD operations 
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const todoController = require('../controllers/todoControllers');
 
 router.post('/', todoController.createTodo);
-// router.get('/', todoController.getTodos);
-// router.get('/:id', todoController.getTodoById);
+router.get('/', todoController.getTodos);
+router.get('/:id', todoController.getTodoById);
 // router.put('/:id', todoController.updateTodo);
 // router.delete('/:id', todoController.deleteTodo);
 
